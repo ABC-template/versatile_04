dbb7dbb996c9e51de098f2c26cb7f72014a337cd
ABC-template <62172120+ABC-template@users.noreply.github.com>
2026-06-10 12:08:43 +0400

    Update scheduler.js

--- api/organizer/scheduler.js
+++ api/organizer/scheduler.js
@@ -1,4 +1,3 @@
-export const config = { runtime: 'edge' };
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY);
